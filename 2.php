1a82a00c7a6a978e53c19673b669bbf881dde357
andreulaso <andreulaso@gmail.com>
2021-07-28 23:40:10 +0300

    Добавлена функция convertString

--- 2.php
+++ 2.php
@@ -1,2 +1,7 @@
 <?php
 declare(strict_types=1);
+
+function convertString(string &$a, string $b): void {
+
+	$a = preg_replace_callback('/'.$b.'/', function ($m) use (&$count) {$count++; return $count == 2 ? strrev($m[0]) : $m[0];}, $a, 2);	
+}
