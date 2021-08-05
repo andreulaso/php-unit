f3bfef1d210412f398e04c38d38d875ff44ca2de
andreulaso <andreulaso@gmail.com>
2021-08-05 19:47:17 +0300

    Добавлена функция importXml

--- 2.php
+++ 2.php
@@ -13,7 +13,7 @@ function convertString(string &$a, string $b): void {
 
 function mySortForKey(array &$a, string $b): void {
 
-	uksort($a, function($f, $s) use ($b, $a) {
+	uksort($a, function($f, $s) use ($a, $b) {
 
 		if (isset($a[$f][$b]))
 		{
@@ -24,5 +24,113 @@ function mySortForKey(array &$a, string $b): void {
 			} throw new Exception('Array['.($s).'] не содержит индекс '.$b);			
 		
 		} throw new Exception('Array['.$f.'}] не содержит индекс '.$b);
-   });
+	});
+}
+
+
+function importXml(string $a): void {
+	
+	if (file_exists($a)) {
+		
+		$xml = simplexml_load_file($a);
+
+		if ($xml !== FALSE) {
+
+			if ($xml->Товар->count() > 0){
+			
+				$con = mysqli_connect('localhost' ,'root' ,'140905' ,'test_samson');
+
+				if (!mysqli_connect_errno($con)){
+
+					mysqli_set_charset($con, "utf8");
+
+					foreach ($xml->Товар as $product) {
+
+						$code = $product->attributes()->Код;
+						$name = $product->attributes()->Название;
+						
+						if (isset($code, $name)){	
+
+							$sql = "INSERT INTO `a_product`(`product_code`,`product_name`) 
+									VALUES ('{$code}', '{$name}')";
+							
+							if (mysqli_query($con, $sql)){ 						
+						
+								foreach ($product->Цена as $price) {
+									
+									$type = $price->attributes()->Тип;
+									
+									if (isset($type) && !empty((string)$price)){
+
+										$sql = "INSERT INTO `a_price`(`product_code`,`price_type`,`price`) 
+												VALUES ('{$code}', '{$type}', '{$price}')";  
+										
+										if (!mysqli_query($con, $sql)){
+											
+											echo ("<pre>Не удалось добавить цену $type для товара $name. " . mysqli_error($con));
+										}
+										
+									} else echo("<pre>Не указан тип цены или цена для товара $name.");
+								}
+
+								foreach ($product->Свойства->children() as $property) {
+									
+									$property_type = $property->getName();
+									
+									$property_unit = $property->attributes()->ЕдИзм;
+									
+									if (isset($property_type) && !empty((string)$property)){
+
+										$sql = "INSERT INTO `a_property`(`product_code`,`property_type`,`property_unit`,`property_value`) 
+												VALUES ('{$code}', '{$property_type}', '{$property_unit}', '{$property}')";  
+										
+										if (!mysqli_query($con, $sql)){
+											
+											echo ("<pre>Не удалось добавить свойство $property_type для товара $name." . mysqli_error($con));
+										}
+										
+									} else echo("<pre>Не удалось добавить свойства для товара $name.");	
+									
+								}		
+
+								foreach ($product->Разделы->children() as $value) {
+									
+									$category = strval($value);
+									
+									if (!empty($category)){
+
+										$sql = "SELECT `category_id` FROM `test_samson`.`a_category` 
+												WHERE category_name = '{$category}' LIMIT 1"; 	
+
+										$id = mysqli_fetch_array( mysqli_query($con, $sql));
+
+										if ($id){
+
+											$sql = "INSERT INTO `a_product_category`(`product_code`,`category_id`) 
+													VALUES ('{$code}', '{$id['category_id']}')";
+
+											if (!mysqli_query($con, $sql)){
+												
+												echo ("<pre>Не удалось добавить раздел $category для товара $name." . mysqli_error($con));
+											}
+										
+										} else echo ("<pre>Раздела $category не существует." . mysqli_error($con));									
+
+									} else echo("<pre>Не удалось добавить раздел для товара $name.");										
+
+								}	
+							
+							} else echo("<pre>Не удалось добавить товар $name. " . mysqli_error($con));		
+						
+						} else echo("<pre>Не указан код или название товара");					
+
+					}
+				
+				} else throw new Exception("<pre>Failed to connect:" . mysqli_connect_error());
+
+			} else throw new Exception("<pre>Не найдены товары");
+
+		} else throw new Exception("<pre>Не удалось прочитать файл $a.");	 		
+
+	} else throw new InvalidArgumentException("<pre>Не удалось открыть файл $a.");		
 }
