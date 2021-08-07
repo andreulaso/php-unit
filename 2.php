7f32cedd83c820e61ecb2e4054205f59ad3d8fc4
andreulaso <andreulaso@gmail.com>
2021-08-07 15:23:00 +0300

    Добавлена функция exportXml

--- 2.php
+++ 2.php
@@ -32,17 +32,19 @@ function importXml(string $a): void {
 	
 	if (!file_exists($a)) throw new InvalidArgumentException("<pre>Не удалось открыть файл $a.");	 
 		
-		$xml = simplexml_load_file($a);
+	$xml = simplexml_load_file($a);
 
-		if ($xml === FALSE)	throw new Exception("<pre>Не удалось прочитать файл $a.");	 
+	if ($xml === FALSE)	throw new Exception("Не удалось прочитать файл $a.");	 
 
-		if ($xml->Товар->count() === 0) throw new Exception("<pre>Не найдены товары");
+	if ($xml->Товар->count() === 0) throw new Exception("Не найдены товары");
 	
-			$con = mysqli_connect('localhost', 'root', '140905', 'test_samson');
+	//подключение к базе данных
+	
+	$con = mysqli_connect('localhost', 'root', '140905', 'test_samson');
 
-			if (mysqli_connect_errno($con))} else throw new Exception("<pre>Failed to connect:" . mysqli_connect_error());
+	if (mysqli_connect_errno($con)) throw new Exception("Failed to connect:" . mysqli_connect_error());
 
-				mysqli_set_charset($con, "utf8");
+	mysqli_set_charset($con, "utf8");
 
 	foreach ($xml->Товар as $product) {
 
@@ -99,23 +101,29 @@ function importXml(string $a): void {
 					
 					if (!empty($category)){
 
-						$sql = "SELECT `category_id` FROM `test_samson`.`a_category` 
+						$sql = "SELECT `category_id` FROM `a_category` 
 								WHERE category_name = '{$category}' LIMIT 1"; 	
 
-						$id = mysqli_fetch_array( mysqli_query($con, $sql));
-
-						if ($id){
-
-							$sql = "INSERT INTO `a_product_category`(`product_code`,`category_id`) 
-									VALUES ('{$code}', '{$id['category_id']}')";
-
-							if (!mysqli_query($con, $sql)){
-								
-								echo ("<pre>Не удалось добавить раздел $category для товара $name." . mysqli_error($con));
-							}
+						$result = mysqli_query($con, $sql);
 						
-						} else echo ("<pre>Раздела $category не существует." . mysqli_error($con));									
+						if ($result){	
+							
+							$id = mysqli_fetch_array($result);
+							
+							if ($id){
 
+								$sql = "INSERT INTO `a_product_category`(`product_code`,`category_id`) 
+										VALUES ('{$code}', '{$id['category_id']}')";
+
+								if (!mysqli_query($con, $sql)){
+									
+									echo ("<pre>Не удалось добавить раздел $category для товара $name." . mysqli_error($con));
+								}
+						
+							} else echo ("<pre>Раздел $category не найден. " . mysqli_error($con));	
+						
+						} else echo ("Не удалось выполнить запрос для раздела $category. " . mysqli_error($con));
+						
 					} else echo("<pre>Не удалось добавить раздел для товара $name.");										
 
 				}	
@@ -123,5 +131,143 @@ function importXml(string $a): void {
 			} else echo("<pre>Не удалось добавить товар $name. " . mysqli_error($con));		
 		
 		} else echo("<pre>Не указан код или название товара");					
-	}		
+	}	
+	
+	echo("<pre>Импорт файла завершен");		
 }
+
+
+function exportXml(string $a, string $b): void {
+	
+	if (empty($a) || empty($b)) throw new InvalidArgumentException("One of the arguments is empty");
+	
+	//подключение к базе данных
+	
+	$con = mysqli_connect('localhost', 'root', '140905', 'test_samson');
+
+	if (mysqli_connect_errno($con)) throw new Exception("Failed to connect:" . mysqli_connect_error());
+
+	mysqli_set_charset($con, "utf8");
+	
+	//выборка id раздела
+	
+	$sql = "SELECT `category_id` FROM `a_category` WHERE category_name = '{$b}' LIMIT 1"; 	
+
+	$result = mysqli_query($con, $sql);
+
+	if ($result){
+		
+		$id = mysqli_fetch_array($result);
+		
+		if ($id){	
+		
+			//выборка всех потомков раздела
+			
+			$sql = "SELECT * FROM `a_category` c 
+					JOIN `a_tree` t ON (c.category_id = t.descendant) 
+					WHERE t.ancestor = '{$id['category_id']}';";
+	
+			$categories = mysqli_query($con, $sql);
+			
+			$products = $category_names = [];
+			
+			//выборка всех товаров входящих в разделы			
+			
+			while ($row = $categories->fetch_assoc()) {
+				
+				$sql = "SELECT `product_code` FROM `a_product_category` 
+						WHERE category_id = '{$row['category_id']}'"; 				
+
+				$product_code = mysqli_query($con, $sql);
+
+				//добавление названий разделов в массив
+				$category_names += [$row['category_id']=>$row['category_name']];
+				
+				//добавление товаров в массив
+				if ($product_code) $products = array_merge($products, mysqli_fetch_all($product_code)); 
+			}
+
+			$products = array_unique($products, SORT_REGULAR);
+			
+			if (count($products) > 0){
+
+				$xml = new SimpleXMLElement("<?xml version=\"1.0\" encoding=\"utf-8\" ?><Товары></Товары>");
+				
+				// добавление товаров в xml
+				
+				foreach ($products as $product_code) {
+					
+					$product = $xml->addChild('Товар');					
+					$product->addAttribute('Код', $product_code[0]);					
+					
+					$childs	=	['Цена'=>'`price_type`,`price` FROM `a_price`',
+								 'Свойства'=>'`property_type`,`property_unit`,`property_value` FROM `a_property`',
+								 'Разделы'=>'`category_id` FROM `a_product_category`'
+								];
+
+					foreach ($childs as $name => $sql) {
+
+						$result = mysqli_query($con, "SELECT ".$sql." WHERE product_code = '{$product_code[0]}'");	
+						
+						if ($result){	
+							
+							if ($name == 'Свойства' || $name == 'Разделы') $property = $product->addChild($name);	
+
+							while ($row = $result->fetch_assoc()) {
+							
+								if ($name == 'Цена'){
+									
+									$child = $product->addChild('Цена', $row['price']);
+									
+									$child->addAttribute('Тип', $row['price_type']);
+								
+								} else if ($name == 'Свойства'){
+									
+									$child = $property->addChild($row['property_type'], $row['property_value']);	
+									
+									if (!empty($row['property_unit'])){
+										
+										$child->addAttribute('ЕдИзм', $row['property_unit']);	
+									}									
+								
+								} else if ($name == 'Разделы'){
+									
+									$property->addChild('Раздел', $category_names[$row['category_id']]);	
+								}
+							}
+						}							
+					}
+				}
+				$dom = dom_import_simplexml($xml)->ownerDocument;				
+				$dom->formatOutput = true;				
+
+				if ($dom->save($a)){
+					
+					echo("<pre>Файл $a успешно создан. {$dom->saveXML()}");
+				
+				} else echo("<pre>Не удалось создать файл $a.");
+
+			} else echo ("<pre>Не найдено ни одного товара.");
+			
+		} else echo ("<pre>Раздел $b не найден. " . mysqli_error($con));
+	
+	} else throw new Exception("Не удалось выполнить запрос. " . mysqli_error($con));			
+}
+
+//importXml('2.xml');
+
+exportXml('export.xml', 'Принтеры и МФУ');
+
+/*РАЗДЕЛЫ
+	(1, 'Компьютеры и офисная техника'),
+		(2, 'Компьютеры и комплектующие'),
+			(9, 'Комплектующие для ПК'),
+			(10, 'Системные блоки'),	
+			(11, 'Хранение данных и охлаждение');		
+		(3, 'Офисная техника и расходные материалы'),		
+			(4, 'Принтеры и МФУ'),
+				(5, 'Принтеры'),
+				(6, 'МФУ'),				
+			(7, 'Расходные материалы'),		
+				(8, 'Бумага'),
+*/
