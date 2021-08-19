bfed3bfa5fe10b7ce882cae7e621300b2a622dcc
andreulaso <andreulaso@gmail.com>
2021-08-19 22:38:56 +0300

    bugs fixed

--- 3.php
+++ 3.php
@@ -3,203 +3,203 @@ namespace Test3;
 
 class newBase
 {
-   // static private $count = 0;
-    private static $count = 0;	
-    //static private $arSetName = [];
-    private static $arSetName = [];	
-    /**
-     * @param string $name
-     */
-    function __construct(string $name = '0')		 
-    //function __construct(int $name = 0)	
-    {
-        if (empty($name)) {
- 			//while (array_search(self::$count, self::$arSetName) != false) {
+	// static private $count = 0;
+	private static $count = 0;	
+	//static private $arSetName = [];
+	private static $arSetName = [];	
+	/**
+	* @param string $name
+	*/
+	function __construct(string $name = '0')		 
+	//function __construct(int $name = 0)	
+	{
+		if (empty($name)) {
+			//while (array_search(self::$count, self::$arSetName) != false) {
 			while (array_search(self::$count, self::$arSetName) !== false) {
-			   ++self::$count;
-            }
-            $name = self::$count;
-        }
-        $this->name = $name;
-        self::$arSetName[] = $this->name;
-    }
-    //private $name;
-    protected $name;	
-    /**
-     * @return string
-     */
-    public function getName(): string
-    {
-        return '*' . $this->name  . '*';
-    }
-    protected $value;
-    /**
-     * @param mixed $value
-     */
-    public function setValue($value)
-    {
-        $this->value = $value;
+				++self::$count;
+			}
+			$name = self::$count;
+		}
+		$this->name = $name;
+		self::$arSetName[] = $this->name;
+	}
+	//private $name;
+	protected $name;	
+	/**
+	* @return string
+	*/
+	public function getName(): string
+	{
+		return '*' . $this->name  . '*';
+	}
+	protected $value;
+	/**
+	* @param mixed $value
+	*/
+	public function setValue($value)
+	{
+		$this->value = $value;
 		//
 		return $this;
-    }
-    /**
-     * @return string
-     */
-    //public function getSize()
-    public function getSize(): string	
-    {
-        $size = strlen(serialize($this->value));
+	}
+	/**
+	* @return string
+	*/
+	//public function getSize()
+	public function getSize(): string	
+	{
+		$size = strlen(serialize($this->value));
 		//return strlen($size) + $size;
 		return $size;
-    }
+	}
 	public function __sleep()	
-    {
-        //return ['value'];
-        return ['name', 'value'];		
-    }
-    /**
-     * @return string
-     */
-    public function getSave(): string
-    {
-        //$value = serialize($value);
-        $value = serialize($this->value);	
-        //return $this->name . ':' . sizeof($value) . ':' . $value;
+	{
+		//return ['value'];
+		return ['name', 'value'];		
+	}
+	/**
+	* @return string
+	*/
+	public function getSave(): string
+	{
+		//$value = serialize($value);
+		$value = serialize($this->value);	
+		//return $this->name . ':' . sizeof($value) . ':' . $value;
 		return $this->name . ':' . self::getSize()  . ':' . $value;		
-    }
-    /**
-     * @return newBase
-     */
-    //static public function load(string $value): newBase
-    public static function load(string $value): newBase	
-    {
-        $arValue = explode(':', $value);
-        return (new newBase($arValue[0]))
+	}
+	/**
+	* @return newBase
+	*/
+	//static public function load(string $value): newBase
+	public static function load(string $value): newBase	
+	{
+		$arValue = explode(':', $value);
+		return (new newBase($arValue[0]))
 			->setValue(unserialize(substr($value, strlen($arValue[0]) + 1
-                //+ strlen($arValue[1]) + 1), $arValue[1]));
-                + strlen($arValue[1]) + 1, $arValue[1])));					
-    }
+				//+ strlen($arValue[1]) + 1), $arValue[1]));
+				+ strlen($arValue[1]) + 1, $arValue[1])));					
+	}
 }
+
 class newView extends newBase
 {
-    private $type = null;
-    private $size = 0;
-    private $property = null;
-    /**
-     * @param mixed $value
-     */
-    public function setValue($value)
-    {
-        parent::setValue($value);
-        $this->setType();
-        $this->setSize();
+	private $type = null;
+	private $size = 0;
+	private $property = null;
+	/**
+	* @param mixed $value
+	*/
+	public function setValue($value)
+	{
+		parent::setValue($value);
+		$this->setType();
+		$this->setSize();
 		//
 		return $this;
-    }
-    public function setProperty($value)
-    {
-        $this->property = $value;
-        return $this;
-    }
-    private function setType()
-    {
-        $this->type = gettype($this->value);
-    }
+	}
+	public function setProperty($value)
+	{
+		$this->property = $value;
+		return $this;
+	}
+	private function setType()
+	{
+		$this->type = gettype($this->value);
+	}
 	private function setSize()
-    {
+	{
 		//if (is_subclass_of($this->value, "Test3\newView")) {
-        if (is_subclass_of($this->value, 'Test3\newBase')) {			
-            $this->size = parent::getSize() + 1 + strlen($this->property);
-        } elseif ($this->type == 'test') {
-            $this->size = parent::getSize();
-        } else {
-            $this->size = strlen($this->value);
-        }
-    }
+		if (is_subclass_of($this->value, 'Test3\newBase')) {			
+			$this->size = parent::getSize() + 1 + strlen($this->property);
+		} elseif ($this->type == 'test') {
+			$this->size = parent::getSize();
+		} else {
+			$this->size = strlen($this->value);
+		}
+	}
 	//
-    public function __sleep()
-    {
+	public function __sleep()
+	{
 		//return ['property'];
-        return ['property', 'value', 'type', 'name', 'size'];		
-    }
-    /**
-     * @return string
-     */
-    public function getName(): string
-    {
-        if (empty($this->name)) {
- 			//throw new Exception('The object doesn\'t have name');           
+		return ['property', 'value', 'type', 'name', 'size'];		
+	}
+	/**
+	* @return string
+	*/
+	public function getName(): string
+	{
+		if (empty($this->name)) {
+			//throw new Exception('The object doesn\'t have name');           
 			throw new \Exception('The object doesn\'t have name');
-        }
-        return '"' . $this->name  . '": ';
-    }
-    /**
-     * @return string
-     */
-    public function getType(): string
-    {
-        return ' type ' . $this->type  . ';';	
-    }
-    /**
-     * @return string
-     */
-    public function getSize(): string
-    {
-        return ' size ' . $this->size . ';';
-    }
-    public function getInfo()
-    {
-        try {
-            echo $this->getName()
-                . $this->getType()
-                . $this->getSize()
-                . "\r\n";
-        //} catch (Exception $exc) {				
-        } catch (\Exception $exc) {
-            echo 'Error: ' . $exc->getMessage();
-        }
-    }
-    /**
-     * @return string
-     */
-    public function getSave(): string
-    {
-		if ($this->type == 'test') {
-            //$this->value = $this->value->getSave();
-        }
-		//var_dump(parent::getSave() . ':' . serialize($this->property));
-        return parent::getSave() . ':' . serialize($this->property);
-    }
-    /**
-     * @return newView
-     */
-    //static public function load(string $value): newBase
+		}
+		return '"' . $this->name  . '": ';
+	}
+	/**
+	* @return string
+	*/
+	public function getType(): string
+	{
+		return ' type ' . $this->type  . ';';	
+	}
+	/**
+	* @return string
+	*/
+	public function getSize(): string
+	{
+		return ' size ' . $this->size . ';';
+	}
+	public function getInfo()
+	{
+		try {
+			echo $this->getName()
+			. $this->getType()
+			. $this->getSize()
+			. "\r\n";
+		//} catch (Exception $exc) {				
+		} catch (\Exception $exc) {
+			echo 'Error: ' . $exc->getMessage();
+		}
+	}
+	/**
+	* @return string
+	*/
+	public function getSave(): string
+	{
+		//if ($this->type == 'test') {
+		//$this->value = $this->value->getSave();
+		//}
+		return parent::getSave() . ':' . serialize($this->property);
+	}
+	/**
+	* @return newView
+	*/
+	//static public function load(string $value): newBase
 	public static function load(string $value): newView	
-    {
-        $arValue = explode(':', $value);
+	{
+		$arValue = explode(':', $value);
 		return (new newView($arValue[0]))	
 			->setValue(unserialize(substr($value, strlen($arValue[0]) + 1
-                //+ strlen($arValue[1]) + 1), $arValue[1]))
-                + strlen($arValue[1]) + 1, $arValue[1])))				
+				//+ strlen($arValue[1]) + 1), $arValue[1]))
+				+ strlen($arValue[1]) + 1, $arValue[1])))				
 			->setProperty(unserialize(substr($value, strlen($arValue[0]) + 1
-                //+ strlen($arValue[1]) + 1 + $arValue[1])))
-                + strlen($arValue[1]) + 1 + $arValue[1] + 1)))				
-            ;			   
-    }
+				//+ strlen($arValue[1]) + 1 + $arValue[1])))
+				+ strlen($arValue[1]) + 1 + $arValue[1] + 1)))				
+			;			   
+	}
 }
 
 function gettype($value): string
 {
 	if (is_object($value)) {
-        $type = get_class($value);
+		$type = get_class($value);
 		do {
-            //if (strpos($type, "Test3\newBase") !== false) {			
+			//if (strpos($type, "Test3\newBase") !== false) {			
 			if (strpos($type, 'Test3\newBase') !== false) {
-                return 'test';
-            }
-        //} while ($type = get_parent_class($type));
-        } while ($type == get_parent_class($value));		
-    }
+				return 'test';
+			}
+			//} while ($type = get_parent_class($type));
+		} while ($type == get_parent_class($value));		
+	}
 	//return gettype($value);  
 	return \gettype($value);
 }
